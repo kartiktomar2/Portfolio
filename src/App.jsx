aa35f839bf38b2fb223aeb8a3d40367f3e59c6cc
Kartik Tomar <ktomar466@gmail.com>
2025-09-02 16:51:17 +0530

    set up particles as background

--- src/App.jsx
+++ src/App.jsx
@@ -10,16 +10,41 @@ import  { Toaster } from 'react-hot-toast';
 import FirstPage from './Components/FirstPage.jsx'
 import { Route, Routes } from 'react-router-dom'
 import NotLive from './Components/NotLive.jsx'
+import Particles from './Components/Particles.jsx'
 function App() {
   return (
-    <>
-    <div>
-            <Routes>
-                <Route path='/' element={<FirstPage/>}/>
-                <Route path='/notLive' element={<NotLive/>}/>
-              </Routes>        
-    </div>
-    <Toaster/>
+  <>
+      {/* Particle background */}
+      <div
+        style={{
+          position: 'fixed',
+          top: 0,
+          left: 0,
+          width: '100vw',
+          height: '100vh',
+          zIndex: 0,
+          pointerEvents: 'none',
+        }}
+      >
+        <Particles
+          particleColors={['#7127BA', '#7127BA']}
+          particleCount={300}
+          particleSpread={10}
+          speed={0.1}
+          particleBaseSize={100}
+          moveParticlesOnHover={false}
+          alphaParticles={false}
+          disableRotation={false}
+        />
+      </div>
+
+      <div style={{ position: 'relative', zIndex: 1 }}>
+        <Routes>
+          <Route path='/' element={<FirstPage/>}/>
+          <Route path='/notLive' element={<NotLive/>}/>
+        </Routes>        
+      </div>
+      <Toaster/>
     </>
   )
 }
