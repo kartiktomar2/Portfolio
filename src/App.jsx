3c9c144cd1039b4fbb1942bc64e498142711ff63
Kartik Tomar <ktomar466@gmail.com>
2025-09-04 22:41:34 +0530

    added tranisition while navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,28 @@
-import React from 'react'
-import Navbar from './Components/Navbar.jsx'
-import Home from './Components/Home.jsx'
-import About from './Components/About.jsx'
-import Portfolio from './Components/Portfolio.jsx'
-import Experience from './Components/Experience.jsx'
-import Contact from './Components/Contact.jsx'
-import Footer from './Components/Footer.jsx'
-import  { Toaster } from 'react-hot-toast';
-import FirstPage from './Components/FirstPage.jsx'
-import { Route, Routes } from 'react-router-dom'
-import NotLive from './Components/NotLive.jsx'
-import Particles from './Components/Particles.jsx'
+import React from "react";
+import { Route, Routes, useLocation } from "react-router-dom";
+import { AnimatePresence, motion } from "framer-motion";
+import { Toaster } from "react-hot-toast";
+
+import FirstPage from "./Components/FirstPage.jsx";
+import NotLive from "./Components/NotLive.jsx";
+import Particles from "./Components/Particles.jsx";
+
 function App() {
+  const location = useLocation();
+
   return (
-  <>
+    <>
       {/* Particle background */}
       <div
         style={{
-          position: 'fixed',
-          top: 0,
-          left: 0,
-          width: '100vw',
-          height: '100vh',
+          position: "fixed",
+          inset: 0,
           zIndex: 0,
-          pointerEvents: 'none',
+          pointerEvents: "none",
         }}
       >
         <Particles
-          particleColors={['#7127BA', '#7127BA']}
+          particleColors={["#7127BA", "#7127BA"]}
           particleCount={300}
           particleSpread={10}
           speed={0.1}
@@ -38,15 +33,54 @@ function App() {
         />
       </div>
 
-      <div style={{ position: 'relative', zIndex: 1 }}>
-        <Routes>
-          <Route path='/' element={<FirstPage/>}/>
-          <Route path='/notLive' element={<NotLive/>}/>
-        </Routes>        
+      {/* Page transitions */}
+      <div style={{ position: "relative", zIndex: 1 }}>
+        <AnimatePresence mode="wait">
+          <Routes location={location} key={location.pathname}>
+            <Route
+              path="/"
+              element={
+                <PageTransition>
+                  <FirstPage />
+                </PageTransition>
+              }
+            />
+            <Route
+              path="/notLive"
+              element={
+                <PageTransition>
+                  <NotLive />
+                </PageTransition>
+              }
+            />
+          </Routes>
+        </AnimatePresence>
       </div>
-      <Toaster/>
+
+      <Toaster />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
+/* ✅ Page transition wrapper */
+function PageTransition({ children }) {
+  return (
+    <motion.div
+      initial={{ opacity: 0, x: "-100%" }}   // Enter from left
+      animate={{ opacity: 1, x: 0 }}         // Center with fade
+      exit={{ opacity: 0, x: "100%" }}       // Exit to right with fade
+      transition={{ duration: 0.6, ease: "easeInOut" }}
+      style={{
+        position: "absolute",   // 👈 fixes the top-gap issue
+        top: 0,
+        left: 0,
+        width: "100%",
+        height: "100%",
+      }}
+    >
+      {children}
+    </motion.div>
+  );
+}
